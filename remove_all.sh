a98ae7125d67e8534da3341a484ebd69712b12a9
Biel <bielselsol@gmail.com>
2026-04-07 19:28:03 +0200

    Fixed global remove

--- remove_all.sh
+++ remove_all.sh
@@ -2,7 +2,7 @@
 
 set -e  # Detiene el script si algún comando falla
 
-for dir in Practica_1 Practica_2 Practica_3; do
+for dir in Introduccion Practica_1 Practica_2 Practica_3; do
     echo "=========================="
     echo "Entrando en $dir ..."
     cd "$dir"
