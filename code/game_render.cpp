3eb5f4baa1ca8a019bc67e526bf3e62c3c832483
lkokul <mrobe2503@gmail.com>
2026-04-26 00:44:14 +0200

    si

--- code/game_render.cpp
+++ code/game_render.cpp
@@ -18,7 +18,7 @@
 #include "control_mando.h"
 
 #define NUM_LUCES_SEM 4
-#define IP "http://192.168.1.40:4747/video"
+#define IP "0"
 using namespace std;
 
 /* ============== GLOBAL VARIABLES =============== */
